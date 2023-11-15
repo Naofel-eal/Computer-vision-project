091598961eead2f1dfaa2731f7cdec4c45638ed4
Allan Coutarel <allancoutarel@orange.fr>
2023-11-15 16:48:40 +0100

    Run on cuda

--- classes/model.py
+++ classes/model.py
@@ -1,9 +1,12 @@
 from ultralytics import YOLO
+from torch import cuda
 
 class Model:
     def __init__(self, model_path: str = "model/yolov8/model.pt") -> None:
         self.model = YOLO(model_path)
+        device = 'cuda' if cuda.is_available() else 'cpu'
+        self.model.to(device)
 
     def detect(self, img):
-        result = self.model.predict(img, show=True, save=False)[0].boxes.xyxy.tolist()
-        print(result)
+        result = self.model.predict(img, show=True, save=False)
+        bounding_boxes = result[0].boxes.xyxy.tolist()
