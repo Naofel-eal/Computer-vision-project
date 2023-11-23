de11ba7b2e1a2af82410ef64f0a9607bb9b8c1e8
Allan Coutarel <allancoutarel@orange.fr>
2023-11-23 18:11:54 +0100

    Add blur function (first version - blur non-progressive and px not %)

--- services/image_editor.py
+++ services/image_editor.py
@@ -1,5 +1,6 @@
 from models.bounding_box import BoundingBox
 from numpy import ndarray
+import cv2
 
 class ImageEditor:
     
@@ -10,3 +11,15 @@ class ImageEditor:
         x1 = bounding_box.upper_left.x
         x2 = bounding_box.bottom_right.x
         return frame[y1:y2, x1:x2]
+    
+    @staticmethod
+    def blur(frame: ndarray, bounding_box: BoundingBox, blur_lvl: int) -> ndarray:
+        # Idéalement mettre zone de flou sup. en % et flou progressif
+        y1 = max(0, bounding_box.upper_left.y - 15)
+        y2 = min(frame.shape[0], bounding_box.bottom_right.y + 15)
+        x1 = max(0, bounding_box.upper_left.x - 15)
+        x2 = min(frame.shape[1], bounding_box.bottom_right.x + 15)
+        cropped_image = frame[y1:y2, x1:x2]
+        blur_cropped_image = cv2.GaussianBlur(cropped_image, (blur_lvl, blur_lvl), 0)
+        frame[y1:y2, x1:x2] = blur_cropped_image
+        return frame
