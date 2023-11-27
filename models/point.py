7d52a39a53e909dc7754c4e84a748f221bc5fe0b
Allan Coutarel <allancoutarel@orange.fr>
2023-11-27 14:11:34 +0100

    Change blur size : % of bounding_box

--- models/point.py
+++ models/point.py
@@ -3,7 +3,8 @@ class Point:
     def __init__(self, x: int, y: int):
         self.x: int = int(x)
         self.y: int = int(y)
-
-    def __str__(self) -> str:
-        return f"({self.x}, {self.y})"
-    
+        
+    def __sub__(self, P2: 'Point') -> [float, float]:
+        width = abs(self.x - P2.x)
+        height = abs(self.y - P2.y)
+        return (width, height)
--- services/image_editor.py
+++ services/image_editor.py
@@ -15,12 +15,14 @@ class ImageEditor:
     
     @staticmethod
     def blur(frame: ndarray, bounding_box: BoundingBox, blur_lvl: int) -> ndarray:
-        # Idéalement mettre zone de flou sup. en % et flou progressif
-        y1 = max(0, bounding_box.upper_left.y - 15)
-        y2 = min(frame.shape[0], bounding_box.bottom_right.y + 15)
-        x1 = max(0, bounding_box.upper_left.x - 15)
-        x2 = min(frame.shape[1], bounding_box.bottom_right.x + 15)
-        cropped_image = frame[y1:y2, x1:x2]
+        bounding_box_sizes = bounding_box.upper_left - bounding_box.bottom_right
+        width = bounding_box_sizes[0]
+        height = bounding_box_sizes[1]
+        y1 = max(0, int(bounding_box.upper_left.y - height * 0.15))
+        y2 = min(frame.shape[0], int(bounding_box.bottom_right.y + height * 0.15))
+        x1 = max(0, int(bounding_box.upper_left.x - width * 0.15))
+        x2 = min(frame.shape[1], int(bounding_box.bottom_right.x + width * 0.15))
+        cropped_image = ascontiguousarray(frame[y1:y2, x1:x2])
         blur_cropped_image = cv2.GaussianBlur(cropped_image, (blur_lvl, blur_lvl), 0)
         frame[y1:y2, x1:x2] = blur_cropped_image
         return frame
