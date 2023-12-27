c827e8df0b109a4b40e5391143d74bcf954b9a2b
a-coutarel <allancoutarel@orange.fr>
2023-12-27 21:12:23 +0100

    Add gradio image interface

--- interfaces/image_interface.py
+++ interfaces/image_interface.py
@@ -1,14 +1,72 @@
 import gradio as gr
 
+from services.medias.image_processor import ImageProcessor
+
 class ImageInterface:
     
     def __init__(self):
-        self.image_tab = gr.Interface(
-            fn=self.process_image,
-            inputs=["image"],
-            outputs=["image"]
-        )
+        self.image_tab = gr.Blocks()
+        self.image_processor = ImageProcessor()
+        self.personsDTO = None
+        
+        with self.image_tab:  
+            gr.Markdown(
+                """
+                    # Import your image
+                    Import the image you want to edit from the interface below.
+                """
+            )
+            image_input = gr.Image(label="Input image")
+            analyse_button = gr.Button("Analyze the image")
+                
+            gr.Markdown(
+                """
+                    # Settings
+                    Here you can choose your settings before making changes to the image.
+                """
+            )
+            persons_faces_output = gr.Gallery(label="Detected faces of people", show_label=True, preview=True)
+            checkboxes = gr.CheckboxGroup(label="Select the people you want to blur", info="You can select as many people as you want.")
+            apply_blur_checkbox = gr.Checkbox(label="Gradient blur", info="Check the box bellow if you want to apply a gradient circular blur rather than a raw rectangular blur. (Note that the image processing will take longer with gradient blur)")
+            blur_button = gr.Button("Image processing")
+            
+            gr.Markdown(
+                """
+                    # Output image
+                    You can download the output image by clicking the download icon on the upper right corner of the image.
+                """
+            )
+            image_output = gr.Image(label="Output image")
+            
+            analyse_button.click(self.analyse_image, inputs=image_input, outputs=[persons_faces_output, checkboxes])
+            checkboxes.change(self.update_persons_should_be_blurred, inputs=checkboxes)
+            blur_button.click(self.apply_blur, inputs=[image_input, apply_blur_checkbox], outputs=image_output)
+            image_input.change(self.reset, outputs=[persons_faces_output, checkboxes, image_output])
+            
 
-    def process_image(self, image):
-        # code de traitement de la vidéo ici
+    def analyse_image(self, image):
+        self.personsDTO = self.image_processor.get_persons(frame=image)
+        persons_labels = []
+        persons_faces = []
+        for i, person in enumerate (self.personsDTO):
+            persons_labels.append(f"Person {i}")
+            persons_faces.append((person.face, persons_labels[i]))
+        checkboxes = gr.CheckboxGroup(choices=persons_labels, label="Select the persons you want to blur", info="You can select as many people as you want.")
+        return persons_faces, checkboxes
+
+    def update_persons_should_be_blurred(self, checkboxes):
+        if checkboxes:
+            for personDTO in self.personsDTO:
+                personDTO.should_be_blurred = False
+            for label in checkboxes:
+                index = int(label.replace("Person ", ""))
+                self.personsDTO[index].should_be_blurred = True
+            
+    def apply_blur(self, image, apply_blur_checkbox_value):
+        image = self.image_processor.apply_blur(frame=image, personsDTO=self.personsDTO, gradual=apply_blur_checkbox_value)
         return image
+    
+    def reset(self):
+        self.image_processor.reset()
+        checkboxes = gr.CheckboxGroup(choices=[], value=[], label="Select the persons you want to blur", info="You can select as many people as you want.")
+        return None, checkboxes, None
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from interfaces.video_interface import VideoInterface
 image_tab = ImageInterface().image_tab
 video_tab = VideoInterface().video_tab
 
-iface = gr.TabbedInterface([image_tab, video_tab], ["Image", "Video"])
+iface = gr.TabbedInterface([image_tab, video_tab], ["Image processing", "Video processing"], theme=gr.themes.Soft())
 
 if __name__ == "__main__":
-    iface.launch()
+    iface.launch(show_api=False)
